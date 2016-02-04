dfa00de1ad09298cc1fdf778e91f94dadc972e05
md4 <m.dequa@gmail.com>
2016-02-04 11:58:45 +0100

    Added: endpoints info!

--- controllers/RootController.js
+++ controllers/RootController.js
@@ -1,5 +1,6 @@
 var config = require('../config/config');
 var HTTPMethod = require('../constants/HTTPMethod');
+var configController = require('../controllers/config/controllers');
 
 // exports
 
@@ -7,12 +8,36 @@ module.exports.get = _get();
 
 // private
 
+var _actions = _getActions();
+
 function _get() {
     return {
         method: HTTPMethod.GET,
         url: '/',
         action: function (req, callback) {
-            callback(null, config.api.info);
+            callback(null, {
+                api: config.api.info,
+                actions: _getActions()
+            });
         }
     };
+}
+
+function _getActions() {
+    return Object
+        .keys(configController)
+        .reduce(function (memo, controllerName) {
+            var controller = require('../controllers/' + controllerName + 'Controller');
+            var actions = configController[controllerName];
+            memo[controllerName] = actions.reduce(function (memo, actionConfig) {
+                var actionName = actionConfig.action;
+                var action = controller[actionName];
+                memo[actionName] = {
+                    endpoint: action,
+                    schema: require('../controllers/schemas/input/' + controllerName + 'Input')[actionName]
+                };
+                return memo;
+            }, {});
+            return memo;
+        }, {});
 }
